3ac5f2febbe554f39e7a64c497c4cd3c53d99a09
Nick Halstead <hikahooma@gmail.com>
2019-07-27 17:53:45 +0100

    Update build.js

--- build.js
+++ build.js
@@ -18,7 +18,7 @@ Handlebars.registerHelper('is', function (value, test, options) {
 });
 
 Handlebars.registerHelper('date', function (date) {
-    return moment(date, "YYYY").format('YYYY');
+    return moment(date, "MM-DD-YYYY").format('YYYY');
 });
 
 metalsmith(__dirname)
