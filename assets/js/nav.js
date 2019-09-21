64dddb3339f760fba9e46ff9bf8049a9b2d2bfa8
Nick Halstead <hikahooma@gmail.com>
2019-09-21 18:46:58 +0100

    Update nav.js

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -3,7 +3,7 @@ var setupNavigation = function() {
       // MAKE SURE SUBNAV MARGIN-TOP NOT > 0 ////////////////////////////
 
       if ('#topNav .subnav') {
-        var subnavMarginTop = parseInt(Y.one('#topNav .subnav').getStyle('marginTop'),10);
+        var subnavMarginTop = parseInt(('#topNav .subnav').getStyle('marginTop'),10);
         if (subnavMarginTop > 0) {
           Y.all('#topNav .subnav').setStyle('marginTop', 0);
         }
@@ -13,23 +13,18 @@ var setupNavigation = function() {
       if ('#mobileMenuLink a') {
         ('#mobileMenuLink a').on('click', function(e){
           ('#mobileNav').toggleClass('menu-open');
-        });
-
-        var mobileNav = ('#mobileNav');
-        mobileNav && mobileNav.delegate('click', function(e) {
-          e.currentTarget.ancestor('.folder').toggleClass('folder-open');
-        }, '.folder > label');
-      }
+        }); 
 
       // Break Nav if too long for nav and logo
       this.navBreaker();
 
       this.vertAlignCanvas();
 
-      Y.one(window).on('resize', function () {
+      (window).on('resize', function () {
         this.vertAlignCanvas();
         this.navBreaker();
       }, this);
     }
+};  
 // run slider
 $(document).ready(setupNavigation);
