fd18541c6278f7c63462a1682bb7bee928679e37
Nick Halstead <hikahooma@gmail.com>
2019-09-21 18:48:40 +0100

    Update site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     var width = 0;
-    $('.slide img').each(function() {
+    $('.slide').each(function() {
     width += $(this).outerWidth( true );
     });
     $('.sqs-wrapper').css('width', width + 250);
