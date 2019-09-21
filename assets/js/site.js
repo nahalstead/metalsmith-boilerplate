3118a3757ed224aca039155bb37012d0edffe166
Nick Halstead <hikahooma@gmail.com>
2019-09-21 18:39:10 +0100

    Update site.js
    add animate for slider next?

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
-var width = 0;
-$('.slide img').each(function() {
+    var width = 0;
+    $('.slide img').each(function() {
     width += $(this).outerWidth( true );
+    });
+    $('.sqs-wrapper').css('width', width + 250);
 });
-$('.sqs-wrapper').css('width', width + 250);
+var slideWidth = $('.slide img').each(function() {
+    width += $(this).outerWidth( true );
+    });
+$(document).ready(function() {
+    $('.gallery-next').click(function() {
+        $('.slider').animate({left: '-='+slideWidth}, 500);
+    });
 });
