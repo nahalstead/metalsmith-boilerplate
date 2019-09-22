8b901268094860d0377a03ec81d104c2eca8f263
Nick Halstead <hikahooma@gmail.com>
2019-09-22 10:59:37 +0100

    Update site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -5,11 +5,14 @@ $(document).ready(function() {
     });
     $('.sqs-wrapper').css('width', width + 250);
 });
-var slideWidth = $('.slide').each(function() {
+var slider = $('#slideshow');
+var sliderCount = $('div', slider).length;
+slider.width(sliderCount * sliderWidth);
+var sliderWidth = $('.slide').each(function() {
     width += $(this).outerWidth( true );
     });
 $(document).ready(function() {
     $('.gallery-next').click(function() {
-        $('#slideshow').animate({left: '-='+slideWidth}, 500);
+        $('#slideshow').animate({left: '-='+sliderWidth}, 500);
     });
 });
