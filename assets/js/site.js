8acbf1897879f97c4fa9603bc63039e4f59cd4d7
Nick Halstead <hikahooma@gmail.com>
2019-09-22 16:13:23 +0100

    Update site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -16,20 +16,20 @@ $(document).ready(function() {
   $(document).ready(function() {
     var i = 0;
     $(".sqs-wrapper .slide").each( function() {
-    $(this).attr("data-id", +i);
+    $(this).attr("data-slide-url", +i);
     i++;
     });
   });
 // Set width for .sqs-wrapper
 $(document).ready(function() {
     var width = 0;
-    $('.slide').each(function() {
+    $('.sqs-wrapper .slide').width(function() {
     width += $(this).outerWidth( true );
     });
     $('.sqs-wrapper').css('width', width + 250);
     
 // Initiate slider
-    var sliderWidth = $('.slide').each(function() {
+    var sliderWidth = $('.sqs-wrapper .slide').each(function() {
     width += $(this).outerWidth( true );
     });
     var slider = $('.sqs-wrapper');
