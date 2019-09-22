3988dd8128bc4f12b45af495c700976dc41276f1
Nick Halstead <hikahooma@gmail.com>
2019-09-22 10:32:00 +0100

    Update nav.js

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,21 +1,8 @@
 var setupNavigation = function() {
-
-
       // Mobile Nav /////////////////////////////////////////////////////
        $('#mobileMenuLink a').on('click', function(e){
           $('#mobileNav').toggleClass('menu-open');
         }); 
-
-      // Break Nav if too long for nav and logo
-      this.navBreaker();
-
-      this.vertAlignCanvas();
-
-      $(window).on('resize', function () {
-        this.vertAlignCanvas();
-        this.navBreaker();
-      }, this);
-    };
 };  
 // run slider
 $(document).ready(setupNavigation);
